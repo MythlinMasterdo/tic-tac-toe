a8fa9f8fbd521133b136fcda7ae7e8f3fca1fa05
MythlinMasterdo <miles71397@gmail.com>
2016-12-05 15:01:25 -0800

    work on scoreChecker

--- game.js
+++ game.js
@@ -22,13 +22,39 @@ TicTacToe.prototype = {
     }
   },
   boardChecker: function() {
+    var xo = [];
+    var winChecker = [];
+    var player;
+    var char;
     if(this.turn === this.player1) {
-
+      player = this.player1;
+      char = 'X';
     } else {
-
+      player = this.player2;
+      char = 'O';
+    }
+    for(var i = 0; i < this.board.length; i++) {
+      for(var j = 0; j < this.board[i].length; j++) {
+        if(this.board[i][j] === char) {
+          xo.push([i,j]);
+        }
+      }
+    }
+    for(var k = 0; k < xo.length; k++) {
+      if(xo.length < 3) {
+        console.log('No winner yet');
+      } else {
+        if(xo[i][0] === 0) {
+          winChecker.push(0);
+        }
+      }
     }
   }
 }
 
 var game = new TicTacToe();
 game.turns([0,2]);
+game.turns([1,2]);
+game.turns([0,1]);
+game.turns([2,0]);
+game.boardChecker();

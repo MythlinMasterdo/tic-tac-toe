7523883c4d0e44463c62ea26eb948e234ac679ac
MythlinMasterdo <miles71397@gmail.com>
2016-12-05 14:51:25 -0800

    implement ticTacToe class and turn method

---
+++ game.js
@@ -0,0 +1,27 @@
+var TicTacToe = function() {
+  this.player1 = 'player1';
+  this.player2 = 'player2';
+  this.board = [['#','#','#'],
+                ['#','#','#'],
+                ['#','#','#']]
+  this.turn = this.player1;
+}
+
+TicTacToe.prototype = {
+  turns: function(turn) {
+    if(this.turn === this.player1) {
+      var row = this.board[turn[0]];
+      row[turn[1]] = 'X';
+      console.log(this.board, 'Your turn Player2');
+      this.turn = this.player2;
+    } else {
+      var row = this.board[turn[0]];
+      row[turn[1]] = 'X';
+      console.log(this.board, 'Your turn Player1');
+      this.turn = this.player1;
+    }
+  }
+}
+
+var game = new TicTacToe();
+game.turns([0,2]);
